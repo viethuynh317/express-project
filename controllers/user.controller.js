d88a31d80a486807596f3175d7d34e28ba06940b
VietHuynh <huynhvanviet317@gmail.com>
2020-05-29 00:08:00 +0700

    modify-server-side validation (Login)

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -32,6 +32,20 @@ module.exports.view = function(req, res){
 
 module.exports.postCreate = function(req, res){
 	req.body.id = shortid.generate();
+	var errors = [];
+	if (!req.body.name){
+		errors.push('Name is required');
+	}
+	if (!req.body.phone){
+		errors.push('Phone is required');
+	}
+	if(errors.length){ 
+		res.render('hello/create', {
+			errors: errors,
+			values: req.body
+		});
+		return;
+	}
 	db.get('users').push(req.body).write();
 	res.redirect('/users');
 };
