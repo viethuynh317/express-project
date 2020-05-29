fea6dc47d203fab2ffabd9df5afaf8dae85bb212
VietHuynh <huynhvanviet317@gmail.com>
2020-05-30 01:04:29 +0700

    Build authenticate login

--- index.js
+++ index.js
@@ -4,14 +4,16 @@ var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var port = 3000;
 var userRoute = require('./routes/user.route');
+var authRoute = require('./routes/auth.route');
+var authMiddleware = require('./middleware/auth.middleware');
 app.set('view engine', 'pug');
 app.set('views', './views');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static('public'));		
-app.use('/users', userRoute);
-
+app.use('/users', authMiddleware.auth, userRoute);
+app.use('/auth', authRoute);
 app.listen(port, function(){
 	console.log('Server listening on port ' + port);
 });
